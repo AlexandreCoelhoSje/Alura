d53a7c77b163f8041987651c9c8fa16655a3208a
Alexandre Coelho da Silva <47395641+AlexandreCoelhoSje@users.noreply.github.com>
2022-04-11 08:05:34 -0300

    inicio cadastro tarefa (Estavel)

---
+++ VueApp/gerenciador-projetos/src/components/modules/tarefa/TarefaFormulario.vue
@@ -0,0 +1,16 @@
+<script>
+import Tarefa from "../../../domain/tarefa/Tarefa";
+import TarefaService from "@/domain/tarefa/TarefaService";
+
+import PaginaListaPadraoVue from "@/components/shared/layout/paginaListaPadrao.vue";
+import ExibicaoGradeVue from "@/components/shared/control/exibicaoGrade.vue";
+import ModalConfirmacaoVue from '@/components/shared/control/modalConfirmacao.vue';
+
+export default {
+    data() {
+        return {
+
+        };
+    }
+}
+</script>
